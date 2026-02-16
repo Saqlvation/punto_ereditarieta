af1e98fd5469a7f7707a3d05376be12fe6157014
Michael <92533988+Saqlvation@users.noreply.github.com>
2026-02-16 13:39:44 +0100

    Add tests for PuntoOrientato class methods

--- TestPuntoOrientato.java
+++ TestPuntoOrientato.java
@@ -1,6 +1,7 @@
 public class TestPuntoOrientato {
 public static void main(String[] args) {
      PuntoOrientato d = new PuntoOrientato(2,4,"sinistra");
+    
      System.out.println(d);
      d.sposta(5);
      System.out.println(d);
@@ -8,7 +9,8 @@ public static void main(String[] args) {
 
     d.ruotaDestra();
     System.out.println(d);
-   
+    PuntoOrientato e = new PuntoOrientato(-3,4,"basso");
+    System.out.println(e.equals(d));   
 
 
 }
